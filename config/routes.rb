43102c5ca5ae82aa22f655b425d6c4f0d80885f8
Alexander Urizar <urizaraj@gmail.com>
2018-03-19 14:33:10 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'application#hello'
   get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
+  post '/login', to: 'sessions#create', as: 'login'
   post '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
